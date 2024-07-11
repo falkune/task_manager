d1c4aab2c2b5b51e253f900e037216f2339ffb86
falkune <iboudiokel@gmail.com>
2024-07-11 16:54:41 +0200

    update

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -11,42 +11,10 @@
         </span>
         <a class="navbar-brand" href="http://localhost/task_manager/?url=task_list">Mes Tâches</a>
     </p>
-    <!-- <p class="d-flex w-50 m-3">
-        <span class="mx-2">
-            <i class="fa-solid fa-plus"></i>
-        </span>
-        <a class="navbar-brand" href="http://localhost/task_manager/?url=add_task">Ajouter Tâches</a>
-    </p> -->
-    <!-- <p class="d-flex w-50 m-3">
-        <span class="mx-2">
-            <i class="fa-solid fa-users-line"></i>
-        </span>
-        <a class="navbar-brand" href="http://localhost/task_manager/?url=my_team">Mon Equipe</a>
-    </p> -->
     <p class="d-flex w-50 m-3">
         <span class="mx-2">
             <i class="fa-solid fa-user"></i>
         </span>
         <a class="navbar-brand" href="http://localhost/task_manager/?url=profil">Mon Profil</a>
     </p>
-    <?php if(isset($_SESSION['user_info']) && $_SESSION['user_info']['statut'] == "Admin"){ ?>
-        <p class="d-flex w-50 m-3">
-            <span class="mx-2">
-                <i class="fa-solid fa-plus"></i>
-            </span>
-            <a class="navbar-brand" href="http://localhost/task_manager/?url=add_team">Creer Equipe</a>
-        </p>
-        <p class="d-flex w-50 m-3">
-            <span class="mx-2">
-                <i class="fa-solid fa-list"></i>
-            </span>
-            <a class="navbar-brand" href="http://localhost/task_manager/?url=list_team">Liste Equipes</a>
-        </p>
-        <p class="d-flex w-50 m-3">
-            <span class="mx-2">
-                <i class="fa-solid fa-list"></i>
-            </span>
-            <a class="navbar-brand" href="http://localhost/task_manager/?url=list_user">Liste Users</a>
-        </p>
-    <?php } ?>
 </div>
--- views/dashboard.php
+++ views/dashboard.php
@@ -3,34 +3,36 @@
 <div class="main d-flex">
     <?php require_once "includes/sidebar.php"; ?>
     <div class="main-dashboard w-75 m-auto">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <div class="card shadow-sm bg-success text-white rounded-lg">
-                    <div class="card-body text-center">
-                        <i class="fa-solid fa-list"></i>
-                        <a class="navbar-brand" href="http://localhost/task_manager/?url=list_team"><h5 class="card-title font-weight-bold">List Teams</h5></a>
+        <?php if(isset($_SESSION['user_info']) && $_SESSION['user_info']['statut'] == "Admin"){ ?>
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card shadow-sm bg-success text-white rounded-lg">
+                        <div class="card-body text-center">
+                            <i class="fa-solid fa-list"></i>
+                            <a class="navbar-brand" href="http://localhost/task_manager/?url=list_team"><h5 class="card-title font-weight-bold">List Teams</h5></a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-4 mb-4">
-                <div class="card shadow-sm bg-primary text-white rounded-lg">
-                    <div class="card-body text-center">
-                        <i class="fa-solid fa-plus"></i>
-                        <a class="navbar-brand" href="http://localhost/task_manager/?url=add_team"><h5 class="card-title font-weight-bold">Add New Team</h5></a>
+                <div class="col-md-4 mb-4">
+                    <div class="card shadow-sm bg-primary text-white rounded-lg">
+                        <div class="card-body text-center">
+                            <i class="fa-solid fa-plus"></i>
+                            <a class="navbar-brand" href="http://localhost/task_manager/?url=add_team"><h5 class="card-title font-weight-bold">Add New Team</h5></a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-4 mb-4">
+                    <div class="card shadow-sm bg-warning text-white rounded-lg">
+                        <div class="card-body text-center">
+                            <i class="fa-solid fa-list"></i>
+                            <a class="navbar-brand" href="http://localhost/task_manager/?url=list_user"><h5 class="card-title font-weight-bold">List Users</h5></a>
+                        </div>
                     </div>
                 </div>
             </div>
-            
-            <div class="col-md-4 mb-4">
-                <div class="card shadow-sm bg-warning text-white rounded-lg">
-                    <div class="card-body text-center">
-                        <i class="fa-solid fa-list"></i>
-                        <a class="navbar-brand" href="http://localhost/task_manager/?url=list_user"><h5 class="card-title font-weight-bold">List Users</h5></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php } ?>
     </div>
 </div>
 
